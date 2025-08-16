3775e897a5d9a71c5982221389d6f35bd19050be
Lucas-S-S-Cunha <lucassasocuna@gmail.com>
2025-08-16 13:06:05 -0300

    Obtencao dos valores e simbolos

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let wantedCryptos = ["BTCUSDT", "ETHUSDT", "LINKUSDT", "LTCUSDT"]
 
 async function valueReturn() {
-    const res = await fetch("https://api.binance.com/api/v3/ticker/price"); // precisa do await
-    const data = await res.json(); // aguarda a conversão para JSON
+    const res = await fetch("https://api.binance.com/api/v3/ticker/price"); 
+    const data = await res.json(); 
     const dataFiltered = data.filter(e => wantedCryptos.includes(e.symbol))
     console.log(dataFiltered);
 }
